2fad39f689dcd6b2eaa67702865648a3b0a95d5e
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-29 14:40:21 +0300

    back to the normal one

--- controllers/purchaseControllers.js
+++ controllers/purchaseControllers.js
@@ -1,5 +1,5 @@
-import firebase from '../../firebase.js';
-import { getDatabase, set, ref, get, push } from 'firebase/database';
+import firebase from "../firebase.js";
+import { getDatabase, set, ref, get, push } from "firebase/database";
 
 const db = getDatabase(firebase);
 
